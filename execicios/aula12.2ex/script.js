359b2c924f2fbdf23f3b65c7968648bdca94baa0
Pedro Henrique <92455469+eipedrohenrique@users.noreply.github.com>
2022-03-30 14:20:08 -0300

    idade

---
+++ execicios/aula12.2ex/script.js
@@ -0,0 +1,46 @@
+function verificar() {
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fano = document.getElementById('txtano')
+    var res = document.getElementById('res')
+    if (fano.value.length == 0 || Number(fano.value) > ano) {
+        alert('[ERRO] Verifique os dados e tente novamente!')
+    } else {
+        var fsex = document.getElementsByName('radsex')
+        var idade = ano - Number(fano.value)
+        var gênero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
+        if (fsex[0].checked) {
+            gênero = 'Homem'
+            if (idade >=0 && idade <10) {
+                //Criança
+            } else if (idade < 21) {
+                //Jovem
+                img.setAttribute('src', 'jovem-homem.png')
+            } else if (idade < 50) {
+                //Adulto
+                img.setAttribute('src', 'homem-adulto.png')
+            } else {
+                //Idoso
+            }
+        } else if (fsex[1].checked) {
+            gênero = 'Mulher'
+            if (idade >=0 && idade <10) {
+                //Criança
+            } else if (idade < 21) {
+                //Jovem
+                img.setAttribute('src', 'jovem-mulher.png')
+            } else if (idade < 50) {
+                //Adulto
+                img.setAttribute('src', 'mulher-adulta.png')
+            } else {
+                //Idoso
+            }
+        }
+        res.style.textAlign = 'center'
+        res.innerHTML = `Detectamos ${gênero} com ${idade} anos.`
+        res.appendChild(img)
+    }
+
+}
