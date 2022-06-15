e4eb4abc1382a04a86524bd3ddabe176434874b8
Pedro Henrique <92455469+eipedrohenrique@users.noreply.github.com>
2022-06-15 15:29:50 -0300

    lista

--- execicios/aula 20/script.js
+++ execicios/aula 20/script.js
@@ -1,50 +1,11 @@
-var enterButton = document.getElementById("enter");
-var input = document.getElementById("userInput");
-var ul = document.querySelector("ul");
-var item = document.getElementsByTagName("li");
+const texto = document.querySelector('input')
+const btnInsert = document.querySelector('.divInsert button')
+const btnDeleteAll = document.querySelector('.header button')
+const ul = document.querySelector('ul')
 
-function inputLength() {
-    return input.value.length;
+var itensDB = []
+
+btnDeleteAll.onclick = () => {
+    itensDB = []
+    updateDB()
 }
-
-function listLength() {
-    return item.length;
-}
-
-function createListElement() {
-    var li = document.createElement("li");
-    li.appendChild(document.createTextNode(input.value));
-    ul.appendChild(li);
-    input.value = "";
-}
-
-function crossOut() {
-    li.classList.toggle("done");
-}
-
-li.addEventListener("click",crossOut);
-
-var dBtn = document.createElement("button");
-dBtn.appendChild(document.createTextNode("X"));
-li.appendChild(dBtn);
-dBtn.addEventListener("click", deleteListItem);
-
-function deleteListItem() {
-    li.classList.add("delete")
-}
-
-function addListAfterClick() {
-    if (inputLength() > 0) {
-        createListElement();
-    }
-}
-
-function addListAfterKeypress(event) {
-    if (inputLength() > 0 && event.which === 13) {
-        createListElement();
-    }
-}
-
-enterButton.addEventListener("click", addListAfterClick);
-
-input.addEventListener("keypress", addListAfterKeypress);
